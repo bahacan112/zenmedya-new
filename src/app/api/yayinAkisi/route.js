84f3a903b29bc4a0455e6257cbebb1fae03e0cec
bahacan112 <bzenbil19@gmail.com>
2024-09-20 01:22:56 +0300

    yayin akışı eklendi- güncellendi

--- src/app/api/yayinAkisi/route.js
+++ src/app/api/yayinAkisi/route.js
@@ -1,27 +1,25 @@
 // app/api/yayinAkisi/route.js
-import puppeteer from "puppeteer";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 export async function GET() {
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://www.bloomberght.com/yayinakisi");
+    const response = await axios.get("https://www.bloomberght.com/yayinakisi");
+    const $ = cheerio.load(response.data);
 
-    const yayinAkisi = await page.evaluate(() => {
-      const items = Array.from(document.querySelectorAll(".accordionItem"));
-      return items.map((item) => ({
-        time: item.querySelector(".time").innerText.trim(),
-        imgSrc: item.querySelector("img").src,
-        title: item.querySelector("h4").innerText.trim(),
-        description: item.querySelector("p").innerHTML.trim(),
-      }));
-    });
+    const yayinAkisi = $(".accordionItem")
+      .map((index, item) => ({
+        time: $(item).find(".time").text().trim(),
+        imgSrc: $(item).find("img").attr("src"),
+        title: $(item).find("h4").text().trim(),
+        description: $(item).find("p").html().trim(),
+      }))
+      .get();
 
-    await browser.close();
     return new Response(JSON.stringify(yayinAkisi), { status: 200 });
   } catch (error) {
     console.error("Veri alınırken bir hata oluştu:", error);
-    return new Response(JSON.stringify({ error: "111Failed to fetch data" }), {
+    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
       status: 500,
     });
   }
