e42929eb947c674d6c81f1a0a9fb05dd7c495e63
bahacan112 <bzenbil19@gmail.com>
2024-09-20 12:41:51 +0300

    bloomberg yayin akisi eklendi

--- src/app/deneme/page.js
+++ src/app/deneme/page.js
@@ -1,41 +1,91 @@
-// deneme/page.js
-import React from "react";
+// pages/deneme.js
+"use client";
+import { useEffect, useState, useRef } from "react";
+/* import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap stil dosyasını dahil et
+ */ import FooterOne from "@/components/Footer/FooterOne";
+import HeaderOne from "@/components/Header/HeaderOne";
 
-const DenemePage = async () => {
-  try {
-    const res = await fetch("https://zenmedya-new.vercel.app/api/yayinAkisi"); // Yerel geliştirme için
-    if (!res.ok) {
-      throw new Error("Veri alınamadı");
-    }
-    const yayinAkisi = await res.json();
-  } catch {
-    console.log(error);
-  }
+export default function Deneme() {
+  const [programData, setProgramData] = useState(null);
+  const fetchedDataRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/api/yayinAkisi");
+      const data = await response.json();
+      fetchedDataRef.current = data;
+      setProgramData(data);
+    };
+
+    fetchData();
+  }, []);
+  const breadcrumb = {
+    title: "Yayın AkıSı",
+    links: [
+      { name: "Home", href: "/" },
+      { name: "Bloomberg HT", href: "#" },
+    ],
+  };
+
   return (
-    <div>
-      {/*   <h1>Yayın Akışı</h1>
-      {Object.entries(yayinAkisi).map(([day, programs]) => (
-        <div key={day} className="day-section">
-          <h2>{day}</h2>
-          <div className="program-list">
-            {Array.isArray(programs) ? ( // programs bir dizi mi kontrol et
-              programs.map((program, index) => (
-                <div key={index} className="program-item">
-                  {program.imgSrc && (
-                    <img src={program.imgSrc} alt={program.title} />
-                  )}
-                  <h4>{program.title}</h4>
-                  <p>{program.description}</p>
+    <>
+      <main className="main">
+        <HeaderOne data={{ breadcrumb }} />
+        <h3 className="mb-4">Bloomberg HT Program Takvimi</h3>
+        {programData && (
+          <div className="accordion accordion-flush" id="programAccordion">
+            {Object.keys(programData).map((day, index) => (
+              <div className="accordion-item" key={day}>
+                <h2 className="accordion-header" id={`heading${index}`}>
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`#collapse${index}`}
+                    aria-expanded="true"
+                    aria-controls={`collapse${index}`}
+                  >
+                    {day}
+                  </button>
+                </h2>
+                <div
+                  id={`collapse${index}`}
+                  className="accordion-collapse collapse"
+                  aria-labelledby={`heading${index}`}
+                  data-bs-parent="#programAccordion"
+                >
+                  <div className="accordion-body">
+                    <div className="row">
+                      {programData[day].map((program) => (
+                        <div
+                          className="col-lg-3 col-sm-6 mb-4"
+                          key={program.title}
+                        >
+                          <div className="card">
+                            <img
+                              src={program.imgSrc}
+                              alt={program.title}
+                              className="card-img-top"
+                            />
+                            <div className="card-body">
+                              <h3 className="card-title">{program.title}</h3>
+                              <p className="card-text">{program.description}</p>
+                              <p className="card-text text-muted">
+                                {program.time}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </div>
-              ))
-            ) : (
-              <p>No programs available for {day}</p> // Dizi değilse mesaj göster
-            )}
+              </div>
+            ))}
           </div>
-        </div>
-      ))} */}
-    </div>
+        )}
+      </main>
+      <FooterOne />
+    </>
   );
-};
-
-export default DenemePage;
+}
